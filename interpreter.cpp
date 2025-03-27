721dc088d03ac286e501069a24fce6b2defca392
Yohance <ypawania@gmail.com>
2025-03-26 20:34:45 -0400

    Add error checking

--- interpreter.cpp
+++ interpreter.cpp
@@ -1,14 +1,15 @@
 #include <iostream>
 #include <string>
+#include <stack>
 
 const int memSize = 100;
-
 
 int main(){    
     int memory[memSize] = {0};
     int pointer = 0;
     int inputPointer = 0;
-    int loopstart= 0;
+    bool dontExecute = false;
+    std::stack<int> loopStack;
 
     std::string code;
     std::string input;
@@ -17,10 +18,39 @@ int main(){
     std::cin >> input;
     
     for (int i = 0; i < code.length(); ++i){
+        if (dontExecute == true && code[i] != ']'){
+            continue;
+        }
+        if (!loopStack.empty() && i == code.length() - 1){
+            std::cout << "unmatched brackets - '[' at position " << loopStack.top();
+            return 0;
+        }
         switch (code[i]) {
+            case '[':
+                if (memory[pointer] == 0){
+                    dontExecute = true;
+                }
+                loopStack.push(i);
+                break;
+            case ']':
+                if (loopStack.empty()){
+                    std::cout << "unmatched brackets - ']' at position " << i;
+                    return 0;
+                }
+                if (memory[pointer] != 0){
+                    i = loopStack.top();
+                }
+                else {loopStack.pop();}    
+                dontExecute = false;
+                break;
+            
             case '>':
                 if (pointer < memSize - 1) {
                     pointer++;
+                }
+                else {
+                    std::cout << "memory exceeded";
+                    return 0;
                 }
                 break;
             case '<':
@@ -43,17 +73,7 @@ int main(){
                     inputPointer++;
                 }
                 break;
-            // prevent ] before [ and check if current cell is 0 before exeucting
-            case '[':
-                loopstart = i;
-                break;
-            case ']':
-                if (memory[pointer] != 0){
-                    i = loopstart;
-                }
-                break;
             default:
-                // Ignore any other characters
                 break;
         }
     }
